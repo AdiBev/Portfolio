f5f1ec882829e162a79e6a506b8867bbe79320d8
Adi Bev <adityabev@gmail.com>
2019-02-24 15:02:25 +0000

    react-router paths added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
+import { Navbar } from "./nav_bar";
+import { AboutPage } from "./about_page";
+import Portfolio from "./portfolio";
 import Home from "./home";
 
 export const App = () => {
   return (
-    <div>
+    <div className="wrapper">
       <main>
-        <Home />
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/About" component={AboutPage} />
+          <Route path="/Portfolio" component={Portfolio} />
+        </Switch>
       </main>
     </div>
   );
