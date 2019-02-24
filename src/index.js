abae89e3c1311f2babb11939c15092e6fc38ebb1
Adi Bev <adityabev@gmail.com>
2019-02-24 15:05:36 +0000

    AOS library added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import AOS from "aos";
 
 //styles import here
-import Styles from "./styles/main.min.css";
+import Styles from "./styles/main.scss";
+import "aos/dist/aos.css";
 
 import { App } from "./app";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+AOS.init();
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
