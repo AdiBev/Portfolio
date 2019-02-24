1ea4e73e82369e2d2940f6f36e2db3d77c493899
Adi Bev <adityabev@gmail.com>
2019-02-24 15:06:19 +0000

    react-router link added

--- src/nav_bar.js
+++ src/nav_bar.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav>
-      <h2>About</h2>
-      <h2>Portfolio</h2>
+      <Link to="/">
+        <h2>Home</h2>
+      </Link>
+
+      <Link to="/About">
+        <h2>About</h2>
+      </Link>
+
+      <Link to="/Portfolio">
+        <h2>Portfolio</h2>
+      </Link>
     </nav>
   );
 };
